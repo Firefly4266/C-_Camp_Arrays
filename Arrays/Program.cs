b1824ae2c6d2ec779a3e77c785813819aee92b56
Firefly4266 <gaines.johnp@gmail.com>
2017-09-14 03:03:56 -0700

    Problem Try it out pg 82 completed

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,31 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int currentMinimum = Int32.MaxValue;
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] < currentMinimum) currentMinimum = array[index];
+            }
+
+            int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int total = 0;
+            for (int index = 0; index < array2.Length; index++)
+                total += array2[index];
+            float average = (float)total / array2.Length;
+
+            //int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            //int currentMinimum = Int32.MaxValue;
+            //foreach (int arr in array)
+            //{
+            //    if (arr < currentMinimum) currentMinimum = arr;
+            //}
+
+            //int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            //int total = 0;
+            //foreach (int ar in array2)
+            //    total += ar;
+            //float average = (float)total / array2.Length;
         }
     }
 }
