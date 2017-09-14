7edff358cdbdc87cefe791ea1dbf861d8963c4aa
Firefly4266 <gaines.johnp@gmail.com>
2017-09-14 00:22:10 -0700

    Michelle problem 17 completed

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -13,6 +13,13 @@ namespace Arrays
             Console.Write("\n Greetings user, how many words would you like to store? ");
             int userInput = Convert.ToInt32(Console.ReadLine());
             string[] words = new string[userInput];
+            int i = 0;
+            while (i < words.Length)
+            {
+                Console.Write("\n Enter the words you want to store. ");
+                words[i] = Console.ReadLine();
+                i++;
+            }
         }
     }
 }
