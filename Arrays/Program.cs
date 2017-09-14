333ac4bf53b6be50d10f50260225545057d80f00
Firefly4266 <gaines.johnp@gmail.com>
2017-09-14 02:22:17 -0700

    Try it out exercise completed

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,20 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int[] rand = new[] { 1, 2, 64, 6, 987, 5, 9874, 6, 64, 6, 2988, 26, 99, 3345, 89, 98, 10, 31, 77, 73, 17, 46 };
+            int[] tree = new int[rand.Length];
+            Console.WriteLine("\n This is the original array: \n");
+            for (int i = 0; i < rand.Length; i++)
+            {
+                tree[i] = rand[i];
+                Console.Write($" {tree[i]},");
+            }
+            Console.WriteLine("\n\n This is the new array\n");
+            foreach(int t in tree)
+            {
+                Console.Write($" {t},");
+            }
+            Console.WriteLine();
         }
     }
 }
