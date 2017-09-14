a93befeffb587d8fe534c9b41f0358f10046cb95
Firefly4266 <gaines.johnp@gmail.com>
2017-09-13 19:18:19 -0700

    Created array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,9 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.Write("\n Greetings user, how many words would you like to store? ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string[userInput];
         }
     }
 }
